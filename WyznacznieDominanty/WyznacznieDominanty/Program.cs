ca7d2c948826e9dcffac515cd27117928d8e5ea6
Ada Paczocha <ada-paczocha@o2.pl>
2019-02-13 23:26:45 +0100

    refactor

--- WyznacznieDominanty/WyznacznieDominanty/Program.cs
+++ WyznacznieDominanty/WyznacznieDominanty/Program.cs
@@ -10,17 +10,16 @@ namespace WyznacznieDominanty
             List<int> T = new List<int>() { 1, 4, 6, 5, 1, 1, 5, 4 };
             List<int> L = new List<int>();
             List<int> W = new List<int>();
-            int n = T.Count;
 
-            for (int i = 0; i < n; i++)
+            foreach(int number in T)
             {
-                if (L.Contains(T[i]))
+                if (L.Contains(number))
                 {
-                    int index = L.IndexOf(T[i]);
+                    int index = L.IndexOf(number);
                     W[index]++;
                 }
                 {
-                    L.Add(T[i]);
+                    L.Add(number);
                     W.Add(1);
                 }
             }
@@ -34,11 +33,11 @@ namespace WyznacznieDominanty
         {
             int maxValue = w[0];
 
-            for(int i = 1; i< w.Count; i++)
+            foreach(int number in w)
             {
-                if(maxValue < w[i])
+                if (maxValue < number)
                 {
-                    maxValue = w[i];
+                    maxValue = number;
                 }
             }
 
